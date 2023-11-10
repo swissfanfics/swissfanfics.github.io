529b7f87b81ae8f13d8b679c8ce31a45f2669cb3
sk337 <natethemakerreal@gmail.com>
2023-11-10 11:46:31 -0800

    work on index generation

--- build.mjs
+++ build.mjs
@@ -3,7 +3,7 @@ import {
   readdirSync,
   readFileSync,
   lstatSync,
-  realpath,
+  appendFileSync,
 } from "fs";
 import { JSDOM } from "jsdom";
 import MarkdownIt from "markdown-it";
@@ -38,6 +38,7 @@ if (process.env.PRODUCTION) {
   execSync(
     `${exe} build --minify --target=browser --sourcemap=external --outdir=dist src/main.js`
   );
+  appendFileSync("dist/main.js", "\n//# sourceMappingURL=main.js.map");
 } else {
   execSync(
     `${exe} build --minify --target=browser --sourcemap=none --outdir=dist src/main.js`
@@ -54,20 +55,24 @@ const mainPage = new JSDOM(readFileSync(__dirname + "/page_tmpl.html"), {
 function scanDir(dir, mainDom) {
   console.log(`listing ${dir}`);
   var ls = readdirSync("pages/" + dir);
+  var output="";
   for (let i = 0; i < ls.length; i++) {
     var element = ls[i];
     console.log(`checking ${dir}/${element}`);
     var stats = lstatSync(`${__dirname}/pages/${dir}/${element}`);
     if (stats.isDirectory()) {
+
       console.log(`${element} is a dir`);
       execSync(`mkdir dist/${dir}/${element}`);
-      scanDir(`${dir}/${element}`);
+      var scanout = scanDir(`${dir}/${element}`);
+      output+=`<button class="accordian">${element.replaceAll("_", " ")}</button><div class="panel">${scanout}</div>`;
     } else {
+      output+=`<p>${element.replaceAll("_", " ").replace(/\.md$/, "")}</p>`;
       console.log(`${element} is a file`);
       var cont = "" + readFileSync(`${__dirname}/pages/${dir}/${element}`);
       var tmpl = "" + readFileSync(`${__dirname}/page_tmpl.html`);
       var source = md.render(cont);
-      tmpl=tmpl.replace(
+      tmpl=tmpl.replaceAll(
         "${{cnt}}",
         source
       );
@@ -75,9 +80,14 @@ function scanDir(dir, mainDom) {
       writeFileSync(`dist/${dir}/${element.replace(/\.md$/i, ".html")}`, tmpl);
     }
   }
+  return output;
 }
 
 // writeFileSync("dist/index.html", mainPage.serialize());
 
 console.log("running Scan");
-scanDir("", mainPage);
+var fo = scanDir("", mainPage);
+
+var index = ""+readFileSync("page_tmpl.html");
+
+writeFileSync("dist/index.html", index.replaceAll("${{cnt}}", fo));
