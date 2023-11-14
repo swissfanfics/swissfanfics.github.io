6bab3bff3a2a6f5b792e21b3472a2b9d1f66b01e
sk337 <79583517+sk337@users.noreply.github.com>
2023-11-14 10:59:31 -0800

    add font awesome

--- src/main.js
+++ src/main.js
@@ -35,14 +35,14 @@ provideFluentDesignSystem().register(
 
 // import { HighlightJS } from "highlight.js";
 
-// import { library, dom } from "@fortawesome/fontawesome-svg-core";
-// import { fab } from "@fortawesome/free-brands-svg-icons";
-// import { far } from "@fortawesome/free-regular-svg-icons";
-// import { fas } from "@fortawesome/free-solid-svg-icons";
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-// library.add(fab, far, fas);
+library.add(fab, far, fas);
 
-// dom.i2svg();
+dom.i2svg();
 
 const analytics = Analytics({
   app: "swiss",
